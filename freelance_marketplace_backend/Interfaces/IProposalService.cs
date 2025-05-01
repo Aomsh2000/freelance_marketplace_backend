03892f1037dab0e0eb7a84830a727a3bd5804629
Reham-Alsaedi <RehamAlsaedi22@gmail.com>
2025-05-01 18:38:05 +0300

    Updated proposal logic

--- freelance_marketplace_backend/Interfaces/IProposalService.cs
+++ freelance_marketplace_backend/Interfaces/IProposalService.cs
@@ -2,12 +2,12 @@
 
 namespace freelance_marketplace_backend.Interfaces
 {
-	public interface IProposalService
-	{
-		
-		// Method to allow a freelancer to submit a new proposal
-		
-		Task<ProposalDto> SubmitProposalAsync(int projectId, CreateProposalDto proposalDto);
+    public interface IProposalService
+    {
+        // Method to allow a freelancer to submit a new proposal
 
-	}
+        Task<ProposalDto> SubmitProposalAsync(int projectId, CreateProposalDto proposalDto);
+        Task<IEnumerable<ProposalDto>> GetProposalsByFreelancerIdAsync(string freelancerId);
+        Task<bool> DeleteProposalAsync(int proposalId, string freelancerId);
+    }
 }
